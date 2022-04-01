6c743e968223e53a3e3ce0d505fa42e65b9aa595
EnzoACL <afdm@postal6.academiapostal.es.local>
2022-04-01 13:28:00 +0200

    Added user registrer input

--- src/PoC/index.mjs
+++ src/PoC/index.mjs
@@ -18,3 +18,56 @@ function updateButtonClickHandler() {
 }
 
 htmlUpdateButton.addEventListener("click", updateButtonClickHandler)
+//PoC endpoints:
+//post users
+const inputButton = document.querySelector("#userPostButton")
+inputButton.addEventListener("click", userLogHandler)
+async function userPostLog(url, data) {
+    const response = await fetch(
+        url,
+        {
+            method: 'POST',
+            body: data,
+            headers: {
+                "Content-Type": "application/json",
+            }
+        }
+    );
+    const responseData = await response.json();
+    return responseData;
+}
+
+const usuarioDatos = JSON.stringify({userName: "Ejemplo351", password: "abc123"});
+const urlLink = (host + "/login/")
+function userLogHandler() {
+    userPostLog(urlLink, usuarioDatos)
+}
+
+//get messages
+
+const showMessages = document.querySelector("#getMessages")
+
+
+const getMessageButton = document.querySelector("#getMessagesButton")
+
+getMessageButton.addEventListener("click", getMessagesHandler)
+function getMessagesHandler(){
+
+}
+
+
+async function authGet(url, token) {
+    const response = await fetch(
+        url,
+        { 
+            headers: {
+                Authorization: token
+            }
+        }
+    );
+    const data = await response.json();
+    return data;
+}
+
+const messageUrl = (url + "/messages/")
+const messageToken = ()
