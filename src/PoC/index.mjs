8ea334bbbd751f97e245f81fe97f822b7bdafba2
EnzoACL <afdm@postal6.academiapostal.es.local>
2022-04-04 10:31:46 +0200

    Post message and  get message done

--- src/PoC/index.mjs
+++ src/PoC/index.mjs
@@ -19,7 +19,7 @@ function updateButtonClickHandler() {
 
 htmlUpdateButton.addEventListener("click", updateButtonClickHandler)
 //PoC endpoints:
-//post users
+//Post users
 const inputButton = document.querySelector("#userPostButton")
 inputButton.addEventListener("click", userLogHandler)
 async function userPostLog(url, data) {
@@ -37,25 +37,19 @@ async function userPostLog(url, data) {
     return responseData;
 }
 
-const usuarioDatos = JSON.stringify({userName: "Ejemplo351", password: "abc123"});
+const usuarioDatos = JSON.stringify({userName: "Ejemplo3351", password: "abc123"});
 const urlLink = (host + "/login/")
 function userLogHandler() {
     userPostLog(urlLink, usuarioDatos)
 }
 
-//get messages
-
-const showMessages = document.querySelector("#getMessages")
-
-
+//get messages con autenticacion
+//Componentes HTML y handler
+const htmlMessages = document.querySelector("#getMessages")
 const getMessageButton = document.querySelector("#getMessagesButton")
-
 getMessageButton.addEventListener("click", getMessagesHandler)
-function getMessagesHandler(){
 
-}
-
-
+// Funcion asincrona para get con autorizacion
 async function authGet(url, token) {
     const response = await fetch(
         url,
@@ -68,6 +62,52 @@ async function authGet(url, token) {
     const data = await response.json();
     return data;
 }
+//Funcion para obtener el token de autenticacion
+function authToken(id, secret) {
+    // En autenticación Basic, usuario y contraseña se separan con ':'
+    const authToken = `${id}:${secret}`;
+    // Y se codifican en Base64
+    const base64token = btoa(authToken);
+    return `Basic ${base64token}`;
+}
+//Constantes para funcion authGet
+const messageToken = authToken("1649059890009", "abc123");
+const messageUrl = (host + "/messages/")
+//Creamos una funcion para ejecutar el authget y sacar los datos como un string
+async function getMessages() {
+    const obtainMessages = await authGet(messageUrl, messageToken);
+    htmlMessages.innerText = JSON.stringify(obtainMessages);
+};
+//Llamamos a la funcion getMessages con el handler
+function getMessagesHandler() {
+    getMessages();
+}
 
-const messageUrl = (url + "/messages/")
-const messageToken = ()
+//Post message
+async function authPost(url, token, data) {
+    const response = await fetch(
+        url,
+        {
+            method: "POST",
+            body: data,
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token
+            }
+        }
+    );
+    const responseData = await response.json();
+    return responseData;
+}
+//Constantes para usar la funcion post
+const postMessageButton = document.querySelector("#postMessagesButton")
+postMessageButton.addEventListener("click", postMessageHandler)
+const urlPostMessage = host + "/message/"
+const postMessageToken = authToken("1649059890009", "abc123");
+const messageContent = JSON.stringify({content: "Tengo una cosa que no funciona."});
+function postMessageHandler() {
+    authPost(host + "/message/", postMessageToken, messageContent);
+}
+
+
+
